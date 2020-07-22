c3693a274bf8c66162f1482ad210ec3b3c946c97
tradou <trad.aidoud@gmail.com>
2020-07-22 16:16:21 +0200

    Add file component and start aniation for the random button

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import RandomButton from './component/RandomButton';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RandomButton />
   </React.StrictMode>,
   document.getElementById('root')
 );
