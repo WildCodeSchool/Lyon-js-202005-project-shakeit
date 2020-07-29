222d80281f81398f393cc7cc7e02f5bc933f0372
justinehell <hell.justine@gmail.com>
2020-07-29 10:05:29 +0200

    creation of container for recipe page

--- src/App.js
+++ src/App.js
@@ -5,53 +5,42 @@ import Main from "./components/Central Page/Main";
 import Header from "./components/Central Page/Header";
 import Search from "./components/Central Page/Search";
 import DisplayCocktails from "./containers/DisplayCocktails";
-import DisplaySearch from "./containers/DisplaySearch"
-
+import DisplaySearch from "./containers/DisplaySearch";
 
 export default function App() {
   return (
-
     <>
-    <GridLayout>
+      <GridLayout>
+        <Header>
+          <img
+            className="logo"
+            src="https://i.ibb.co/3znmZs9/Daco-4332189.png"
+            alt="Daco-4332189"
+            border="0"
+          />
+          <h1>ShakeIt</h1>
+        </Header>
 
-    <Header>
-     
-      <img  className="logo" src="https://i.ibb.co/3znmZs9/Daco-4332189.png" alt="Daco-4332189" border="0"/>
-      <h1>ShakeIt</h1>
-     
-      
-    
-    
-      </Header>
-      
-      <Search>
-      
-      <DisplaySearch />
-     </Search>
-      
-      
-      <Main>
-      
-      <DisplayCocktails />
-      </Main>
-        
-          
-    </GridLayout>
+        <Search>
+          <DisplaySearch />
+        </Search>
 
-    <Footer>
-      
-    
-    <i class="fa fa-2x fa-refresh" aria-hidden="true"></i>
-    <i class="fa fa-2x fa-list" aria-hidden="true"></i>
-    <i  className=" clickable  fa-2x fa fa-heart"  aria-hidden="true" value="test"></i>
-    <i className="fa fa-2x fa-user" aria-hidden="true"></i>
-    {/* <i class="fa fa-2x fa-bars" aria-hidden="true"></i> */}
-   
+        <Main>
+          <DisplayCocktails />
+        </Main>
+      </GridLayout>
 
-    
-    </Footer>
-
+      <Footer>
+        <i class="fa fa-2x fa-refresh" aria-hidden="true"></i>
+        <i class="fa fa-2x fa-list" aria-hidden="true"></i>
+        <i
+          className=" clickable  fa-2x fa fa-heart"
+          aria-hidden="true"
+          value="test"
+        ></i>
+        <i className="fa fa-2x fa-user" aria-hidden="true"></i>
+        {/* <i class="fa fa-2x fa-bars" aria-hidden="true"></i> */}
+      </Footer>
     </>
   );
 }
-
---
+++ src/containers/RecipeMain.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class RecipeMain extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h2>RecipeMain container</h2>;
+  }
+}
+
+export default RecipeMain;
