78500ea3ce7dd1146e3bbbbea2bd3522b329e123
justinehell <hell.justine@gmail.com>
2020-07-29 12:32:28 +0200

    display cocktail info list - ingredient & mesure

--- src/containers/RecipePage.js
+++ src/containers/RecipePage.js
@@ -6,9 +6,12 @@ class RecipePage extends React.Component {
     super(props);
     this.state = {
       dataRecipe: {},
-      isLoading: true,
     };
     this.getRecipe = this.getRecipe.bind(this);
+  }
+
+  componentDidMount() {
+    this.getRecipe();
   }
 
   getRecipe() {
@@ -20,20 +23,65 @@ class RecipePage extends React.Component {
         console.log("data.drinks[0]", data.drinks[0]);
         this.setState({
           dataRecipe: data.drinks[0],
-          isLoading: false,
         });
       });
   }
   render() {
+    let dataRecipe = this.state.dataRecipe;
     return (
       <div>
         <h2>RecipeMain container</h2>
-        <button onClick={this.getRecipe}>Show a recipe</button>
-        {this.state.isLoading ? (
-          <h2>"Loading..."</h2>
-        ) : (
-          <h2>Cocktail : {this.state.dataRecipe.strDrink}</h2>
-        )}
+        <button onClick={this.getRecipe}>Random recipe</button>
+        <h2>Name : {dataRecipe.strDrink}</h2>
+        <img src={dataRecipe.strDrinkThumb} alt="Cocktail Thumb" />
+        <ul>
+          List of ingredients :
+          <li>
+            {dataRecipe.strMeasure1} {dataRecipe.strIngredient1}
+          </li>
+          <li>
+            {dataRecipe.strMeasure2} {dataRecipe.strIngredient2}
+          </li>
+          <li>
+            {dataRecipe.strMeasure3} {dataRecipe.strIngredient3}
+          </li>
+          <li>
+            {dataRecipe.strMeasure4} {dataRecipe.strIngredient4}
+          </li>
+          <li>
+            {dataRecipe.strMeasure5} {dataRecipe.strIngredient5}
+          </li>
+          <li>
+            {dataRecipe.strMeasure6} {dataRecipe.strIngredient6}
+          </li>
+          <li>
+            {dataRecipe.strMeasure7} {dataRecipe.strIngredient7}
+          </li>
+          <li>
+            {dataRecipe.strMeasure8} {dataRecipe.strIngredient8}
+          </li>
+          <li>
+            {dataRecipe.strMeasure9} {dataRecipe.strIngredient9}
+          </li>
+          <li>
+            {dataRecipe.strMeasure10} {dataRecipe.strIngredient10}
+          </li>
+          <li>
+            {dataRecipe.strMeasure11} {dataRecipe.strIngredient11}
+          </li>
+          <li>
+            {dataRecipe.strMeasure12} {dataRecipe.strIngredient12}
+          </li>
+          <li>
+            {dataRecipe.strMeasure13} {dataRecipe.strIngredient13}
+          </li>
+          <li>
+            {dataRecipe.strMeasure14} {dataRecipe.strIngredient14}
+          </li>
+          <li>
+            {dataRecipe.strMeasure15} {dataRecipe.strIngredient15}
+          </li>
+        </ul>
       </div>
     );
   }
