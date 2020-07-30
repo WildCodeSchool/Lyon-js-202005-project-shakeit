7cb60451eae7011b235ddbbec8a88740fe3b274e
grld <grldfaure@gmail.com>
2020-07-30 15:38:29 +0200

    Random cocktails components

--- src/containers/DisplayCocktails.js
+++ src/containers/DisplayCocktails.js
@@ -1,113 +1,16 @@
 import React from "react";
+import RandomCocktails from "./RandomCocktails.js";
 import "font-awesome/css/font-awesome.min.css";
 
 const DisplayCocktails = () => {
   return (
     <>
       <h2>Cocktails du jour</h2>
-      <div className="cocktailList">
-        <a href="localhost">
-          <img
-            class="logo"
-            src="https://via.placeholder.com/250"
-            alt="Cocktail pic"
-          ></img>
-        </a>
-
-        <div className="cocktailInfos">
-          <p className="cocktailTitle">Title</p>
-          <div className="cocktailRate">
-            <i class="fa fa-star" aria-hidden="true"></i>
-            <i class="fa fa-star" aria-hidden="true"></i>
-            <i class="fa fa-star" aria-hidden="true"></i>
-            <i class="fa fa-star" aria-hidden="true"></i>
-            <i class="fa fa-star" aria-hidden="true"></i>
-          </div>
-          <i
-            className=" clickable fa fa-heart"
-            aria-hidden="true"
-            value="test"
-          ></i>
-        </div>
-      </div>
-
-      <div className="cocktailList">
-        <a href="localhost">
-          <img
-            class="logo"
-            src="https://via.placeholder.com/250"
-            alt="Cocktail pic"
-          ></img>
-        </a>
-
-        <div className="cocktailInfos">
-          <p className="cocktailTitle">Title</p>
-          <div className="cocktailRate">
-            <i class="fa fa-star" aria-hidden="true"></i>
-            <i class="fa fa-star" aria-hidden="true"></i>
-            <i class="fa fa-star" aria-hidden="true"></i>
-            <i class="fa fa-star" aria-hidden="true"></i>
-            <i class="fa fa-star" aria-hidden="true"></i>
-          </div>
-          <i
-            className=" clickable fa fa-heart"
-            aria-hidden="true"
-            value="test"
-          ></i>
-        </div>
-      </div>
-
-      <div className="cocktailList">
-        <a href="localhost">
-          <img
-            class="logo"
-            src="https://via.placeholder.com/250"
-            alt="Cocktail pic"
-          ></img>
-        </a>
-
-        <div className="cocktailInfos">
-          <p className="cocktailTitle">Title</p>
-          <div className="cocktailRate">
-            <i class="fa fa-star" aria-hidden="true"></i>
-            <i class="fa fa-star" aria-hidden="true"></i>
-            <i class="fa fa-star" aria-hidden="true"></i>
-            <i class="fa fa-star" aria-hidden="true"></i>
-            <i class="fa fa-star" aria-hidden="true"></i>
-          </div>
-          <i
-            className=" clickable fa fa-heart"
-            aria-hidden="true"
-            value="test"
-          ></i>
-        </div>
-      </div>
-
-      <div className="cocktailList">
-        <a href="localhost">
-          <img
-            class="logo"
-            src="https://via.placeholder.com/250"
-            alt="Cocktail pic"
-          ></img>
-        </a>
-
-        <div className="cocktailInfos">
-          <p className="cocktailTitle">Title</p>
-          <div className="cocktailRate">
-            <i class="fa fa-star" aria-hidden="true"></i>
-            <i class="fa fa-star" aria-hidden="true"></i>
-            <i class="fa fa-star" aria-hidden="true"></i>
-            <i class="fa fa-star" aria-hidden="true"></i>
-            <i class="fa fa-star" aria-hidden="true"></i>
-          </div>
-          <i
-            className=" clickable fa fa-heart"
-            aria-hidden="true"
-            value="test"
-          ></i>
-        </div>
-      </div>
+      
+          <RandomCocktails />
+          <RandomCocktails />
+          <RandomCocktails />
+       
     </>
   );
 };
