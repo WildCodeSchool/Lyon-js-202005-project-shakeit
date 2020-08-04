728ce2fdfa533f1e099fbe1e13ddf7641006fcda
justinehell <hell.justine@gmail.com>
2020-08-04 12:30:43 +0200

    transform class into functionnal components

--- src/containers/RandomCocktails.js
+++ src/containers/RandomCocktails.js
@@ -1,59 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
-class RandomCocktail extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataRecipe: {},
-    };
-    this.getCocktail = this.getCocktail.bind(this);
-  }
+function RandomCocktail() {
+  const [dataRecipe, setDataRecipe] = useState({});
 
-  componentDidMount() {
-    this.getCocktail();
-  }
-
-  getCocktail() {
+  const getRandomCocktail = () => {
     axios
       .get("https://www.thecocktaildb.com/api/json/v1/1/random.php")
       .then((response) => response.data)
       .then((data) => {
-        this.setState({
-          dataRecipe: data.drinks[0],
-        });
+        setDataRecipe(data.drinks[0]);
       });
-  }
+  };
 
-  render() {
-    let dataRecipe = this.state.dataRecipe;
-    return (
-      <div className="cocktailList">
-        <Link to={`/recipePage/${dataRecipe.strDrink}`}>
-          <img src={dataRecipe.strDrinkThumb} alt="Cocktail Thumb" />
-          {/* <a href="localhost">
-          <img src={dataRecipe.strDrinkThumb} alt="Cocktail Thumb" />
-        </a> */}
-          <div className="cocktailInfos">
-            <p className="cocktailTitle">{dataRecipe.strDrink}</p>
-            <div className="cocktailRate">
-              <i className="fa fa-star" aria-hidden="true"></i>
-              <i className="fa fa-star" aria-hidden="true"></i>
-              <i className="fa fa-star" aria-hidden="true"></i>
-              <i className="fa fa-star" aria-hidden="true"></i>
-              <i className="fa fa-star" aria-hidden="true"></i>
-            </div>
-            <i
-              className=" clickable fa fa-heart"
-              aria-hidden="true"
-              value="test"
-            ></i>
+  useEffect(() => {
+    getRandomCocktail();
+  }, []);
+
+  return (
+    <div className="cocktailList">
+      <Link to={`/recipePage/${dataRecipe.strDrink}`}>
+        <img src={dataRecipe.strDrinkThumb} alt="Cocktail Thumb" />
+        <div className="cocktailInfos">
+          <p className="cocktailTitle">{dataRecipe.strDrink}</p>
+          <div className="cocktailRate">
+            <i className="fa fa-star" aria-hidden="true"></i>
+            <i className="fa fa-star" aria-hidden="true"></i>
+            <i className="fa fa-star" aria-hidden="true"></i>
+            <i className="fa fa-star" aria-hidden="true"></i>
+            <i className="fa fa-star" aria-hidden="true"></i>
           </div>
-        </Link>
-      </div>
-    );
-  }
+          <i
+            className=" clickable fa fa-heart"
+            aria-hidden="true"
+            value="test"
+          ></i>
+        </div>
+      </Link>
+    </div>
+  );
 }
 
 export default RandomCocktail;
