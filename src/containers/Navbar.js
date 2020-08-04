10cd714e4a9ccc8201af184d8ae9f7471050ba6e
Marine Montailler <marinemontailler@gmail.com>
2020-08-04 12:05:17 +0200

    add Link to shoppinglist in the Navbar component

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
       <i className="fa fa-2x fa-refresh" aria-hidden="true"></i>
-      <i className="fa fa-2x fa-list" aria-hidden="true"></i>
+      <Link to="/shoppinglistpage">
+        <i className="fa fa-2x fa-list" aria-hidden="true"></i>
+      </Link>
+      
       <i
         className=" clickable  fa-2x fa fa-heart"
         aria-hidden="true"
