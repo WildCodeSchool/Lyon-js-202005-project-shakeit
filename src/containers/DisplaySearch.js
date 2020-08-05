2daf57fdfc3cd5d156228ad69b6f601ee45fee71
tradou <trad.aidoud@gmail.com>
2020-08-05 12:22:04 +0200

    Auto completion done - WIP: Link value to API in order to display cocktails

--- src/containers/DisplaySearch.js
+++ src/containers/DisplaySearch.js
@@ -1,35 +1,42 @@
-import React from "react";
-import "font-awesome/css/font-awesome.min.css";
+import React, {useState} from "react";
+import 'font-awesome/css/font-awesome.min.css';
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import liste_ingredient from './liste_ingredient.js';
 
-const DisplaySearch = () => {
-  return (
-    <>
-      <hr></hr>
-      <link
-        rel="stylesheet"
-        href="https://unpkg.com/purecss@2.0.3/build/pure-min.css"
-        integrity="sha384-cg6SkqEOCV1NbJoCu11+bm0NvBRc8IYLRGXkmNrqUBfTjmMYwNKPWBTIKyw9mHNJ"
-        crossOrigin="anonymous"
-      ></link>
-      <div>
-        <p className="search__title">Let's find cocktail</p>
-        <div className="searchBar">
-          <input className="search__input" type="text" placeholder="Search" />
-          <i className="fa fa-2x fa-plus" aria-hidden="true"></i>
-        </div>
-      </div>
-
-      <div className="searchTags">
-        <button className="button-secondary pure-button" disabled="">
-          Rhum
-        </button>
-        <button className="button-secondary pure-button" disabled="">
-          Mint
-        </button>
-      </div>
-      <hr></hr>
-    </>
-  );
-};
-
+function DisplaySearch(){
+const [value, setValue] = useState([]);
+const [inputValue, setInputValue] = useState('');
+const Search = ({ value }) => 
+(<button className="button-secondary pure-button" disabled="">{value}</button>)
+        return (
+          <div>
+          <div className="searchBar">
+            <Autocomplete
+              value={inputValue}
+              onChange={(event, newValue) => {
+                setInputValue(newValue);
+              }}
+              options={liste_ingredient.map(option =>option.ingredient)}
+              style={{ width: 300 }}
+              renderInput={(params) => <TextField {...params} label="Search ingredient" variant="outlined" />}
+            />
+            <i onClick={() =>{
+              setValue(value => value.concat(inputValue));
+              console.log(value);}
+            } className="fa fa-2x fa-plus" aria-hidden="true"></i>
+            </div>
+            <div>
+            <ul>
+            {value.map((item, i) => (
+           <Search
+          value={item}
+          key={item}
+          />
+        ))}
+            </ul>
+            </div>
+          </div>
+        );
+      }
 export default DisplaySearch;
