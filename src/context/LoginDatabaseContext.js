fbf36c79c37027daef44b72843b0ee9df9f1352f
Wilder <aurelien.chiren@gmail.com>
2020-08-06 16:14:56 +0200

    adding login and mdp encryption

--- src/context/LoginDatabaseContext.js
+++ src/context/LoginDatabaseContext.js
@@ -1,14 +1,18 @@
 import React, {createContext} from 'react';
 
+
 export const LoginDatabaseContext= createContext();
 
 
 export const LoginDatabaseProvider = (props) => {
 
     const identifiants = {
-        login: "p2",
-        mdp: "p2mdp"
+        login: "1d665b9b1467944c128a5575119d1cfd", //f
+        mdp: "5cef00a3b11989380be79e59b26f8f19"  //
       };
+
+     
+     
 
 
 return(
