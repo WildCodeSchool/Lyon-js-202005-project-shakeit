77b24f35d35e0b0eb6d8c9e8d30a411c72d5bc95
Wilder <aurelien.chiren@gmail.com>
2020-08-06 11:25:54 +0200

    resolving conflict

--- src/containers/PrivateRoute.js
+++ src/containers/PrivateRoute.js
@@ -3,25 +3,7 @@ import { Route, Redirect } from "react-router-dom";
 import { AuthentContext } from "../context/AuthentContext";
 
 function PrivateRoute({ component: Component, ...rest }) {
-  const fakeAuth = useContext(AuthentContext);
-  console.log("Authentifié avant redirection Main?" + fakeAuth.isAuthenticated);
-  return (
-    <Route
-      {...rest}
-      render={(props) =>
-        true ? (
-          <Component {...props} {...rest} />
-        ) : (
-          <Redirect
-            to={{
-              pathname: "/login",
-            }}
-          />
-        )
-      }
-    />
-  );
-}
+  
  
     const [auth,setAuth] = useContext(AuthentContext);
 
