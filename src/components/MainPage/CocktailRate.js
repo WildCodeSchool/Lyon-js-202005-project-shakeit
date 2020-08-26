0df396588741fdefc2d6d89f4ece89deb37c3104
Marine Montailler <marinemontailler@gmail.com>
2020-08-26 14:52:51 +0200

    add css on the cocktail list - searchbar result

--- src/components/MainPage/CocktailRate.js
+++ src/components/MainPage/CocktailRate.js
@@ -1,12 +1,11 @@
 import styled from "styled-components";
 
-
 const CocktailRate = styled.p`
-    display:inline-block;
-    width: 50%;
-    margin-top:5px;
-    margin-bottom:10px;
-    margin-right: 5px;
-`
+  display: inline-block;
+  width: 50%;
+  margin-top: 5px;
+  margin-bottom: 10px;
+  margin-left: 5%;
+`;
 
-export default  CocktailRate
+export default CocktailRate;
