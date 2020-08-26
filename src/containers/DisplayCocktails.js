3bfbccc7c09757c4659f32c36be1172cbd437a50
Marine Montailler <marinemontailler@gmail.com>
2020-08-26 15:34:55 +0200

    develop bug

--- src/containers/DisplayCocktails.js
+++ src/containers/DisplayCocktails.js
@@ -1,19 +1,14 @@
 import React from "react";
-// import RandomCocktails from "./RandomCocktails.js";
+import RandomCocktails from "./RandomCocktails.js";
 
 import "font-awesome/css/font-awesome.min.css";
 
 const DisplayCocktails = () => {
   return (
     <>
-<<<<<<< HEAD
-      <h2>Cocktails du jour</h2>
-      {/* <RandomCocktails />
-=======
       <h2>Random cocktails</h2>
->>>>>>> 1728f29539a573da5c75c365f8f7481141c8b6e9
       <RandomCocktails />
-      <RandomCocktails /> */}
+      <RandomCocktails />
     </>
   );
 };
--- src/containers/DisplaySearch.js
+++ src/containers/DisplaySearch.js
@@ -1,30 +1,15 @@
 import React, { useState, useEffect } from "react";
-<<<<<<< HEAD
-=======
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import CocktailRate from "../components/MainPage/CocktailRate";
 import CocktailList from "../components/MainPage/CocktailList";
->>>>>>> 1728f29539a573da5c75c365f8f7481141c8b6e9
 import "font-awesome/css/font-awesome.min.css";
 import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import liste_ingredient from "./liste_ingredient.js";
 import axios from "axios";
 import { makeStyles } from "@material-ui/core/styles";
-<<<<<<< HEAD
-
-function DisplaySearch() {
-  const [value, setValue] = useState([]);
-  const [valueAPI, setValueAPI] = useState([]);
-  const [testPic, setTestPic] = useState([]);
-  let testAPI = [];
-  let testAPI2 = [];
-  let concTest = [];
-  let pl = [];
-
-=======
 
 function DisplaySearch() {
   const [value, setValue] = useState([]);
@@ -33,7 +18,6 @@ function DisplaySearch() {
   let testAPI = [];
   let testAPI2 = [];
 
->>>>>>> 1728f29539a573da5c75c365f8f7481141c8b6e9
   const useStyles = makeStyles((theme) => ({
     inputRoot: {
       color: "white",
@@ -54,10 +38,6 @@ function DisplaySearch() {
   const classes = useStyles();
 
   useEffect(() => {
-<<<<<<< HEAD
-    console.log(value);
-=======
->>>>>>> 1728f29539a573da5c75c365f8f7481141c8b6e9
     setValueAPI(value);
     value.map((i) =>
       axios
@@ -68,19 +48,6 @@ function DisplaySearch() {
           testAPI2 = testAPI2.concat(
             data["drinks"].map((i) => i.strDrinkThumb)
           );
-<<<<<<< HEAD
-          // pl=pl.concat(data["drinks"].map(i=>i.strDrinkThumb))
-          // pl=pl.concat(data["drinks"].map(i=>i.strDrink))
-          // console.log("ICI",testAPI.map((item,i)=>item[i]));
-          // setValueAPI(testAPI);
-          // console.log("LALALA ",testAPI.map(i=>i[0]));
-          console.log("API ", testAPI);
-          console.log("LAL;ALALA ", testAPI2);
-          console.log(testAPI[1]);
-          concTest.push(testAPI, testAPI2);
-          console.log("BLBLBL", pl);
-=======
->>>>>>> 1728f29539a573da5c75c365f8f7481141c8b6e9
           setValueAPI(testAPI);
           setTestPic(testAPI2);
         })
@@ -102,28 +69,14 @@ function DisplaySearch() {
           <TextField
             {...params}
             variant="outlined"
-<<<<<<< HEAD
-            label="Ingrédients"
-            placeholder="Choisissez vos ingrédients favoris"
-=======
             label="Ingredient"
             placeholder="What's inside your fridge?"
->>>>>>> 1728f29539a573da5c75c365f8f7481141c8b6e9
           />
         )}
       />
       <div>
         {valueAPI.map((item, i) => (
           <div key={i}>
-<<<<<<< HEAD
-            <p>{valueAPI[i]}</p>
-            <img
-              src={testPic[i]}
-              width="100px"
-              height="auto"
-              alt="Cocktail Thumb"
-            />
-=======
             <CocktailList>
               <Link to={`/recipePage/${valueAPI[i]}`}>
                 <img src={testPic[i]} alt="Cocktail Thumb" />
@@ -136,7 +89,6 @@ function DisplaySearch() {
                 <FontAwesomeIcon icon="heart" />
               </Link>
             </CocktailList>
->>>>>>> 1728f29539a573da5c75c365f8f7481141c8b6e9
           </div>
         ))}
       </div>
